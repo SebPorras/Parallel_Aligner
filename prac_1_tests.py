dadc8fd52c48b32c393d962ebfdabfa12db57fd1
Sebastian Porras <103103666+SebPorras@users.noreply.github.com>
2024-11-24 16:40:46 +1000

    Update prac_1_tests.py

--- prac_1_tests.py
+++ prac_1_tests.py
@@ -3,7 +3,7 @@ from answer_template import q1, q2a, q2b, q3a, q3b, q4a, q4b, q5, q6a, q6b, q6c
 
 
 def test_q1():
-    assert q1 == "foo"
+    assert q1 == "foo", "try again!"
 
 def test_q2a():
-    assert q2a == "bar"
+    assert q2a == "bar", "try again!"
